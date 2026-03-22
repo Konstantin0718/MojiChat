e2384140370d7fbcc69f2f79207e44b812bf2e34
Konstantin0718 <konstantin_sabev@abv.bg>
2026-03-22 13:41:48 +0100

    Adjust keyboardVerticalOffset for ChatScreen

--- mobile/src/screens/ChatScreen.tsx
+++ mobile/src/screens/ChatScreen.tsx
@@ -316,7 +316,7 @@ export const ChatScreen: React.FC<Props> = ({ route, navigation }) => {
       <KeyboardAvoidingView
         style={{ flex: 1 }}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 0}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
       >
         {/* Messages */}
         <FlatList
